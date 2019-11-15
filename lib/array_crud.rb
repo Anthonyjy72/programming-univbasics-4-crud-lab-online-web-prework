f08c8bb23621e4feea2021b2516a5be06f831969
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-11-15 17:47:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   colors = ["red", "gold", "black"]
-  colors.push "green"
+  colors.push("green")
 end
 
 def add_element_to_start_of_array(array, element)

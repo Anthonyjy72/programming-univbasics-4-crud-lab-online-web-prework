7af45b2a66899bf147a2546fed485e46e9272ee1
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-11-15 19:14:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  colors = ["arrays!", "red", "gold", "black", "green"]
+  colors = ["wow", "red", "gold", "black", "green"]
   colors.shift
 end
 

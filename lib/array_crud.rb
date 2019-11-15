81058a5c93957bb3f8b341b108406a482c00c8ee
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-11-15 19:19:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  words = ["wow", "cool", "awesome"]
+  words[2]
 end
 
 def update_element_from_index(array, index_number, element)

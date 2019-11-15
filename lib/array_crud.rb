47d79ef369e7a0896cf03ecc1fe1d2c18a53c8ac
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-11-15 17:42:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  colors_array = ["red, green, black, gold"]
+  colors = ["red, green, black, gold"]
 end
 
 def add_element_to_end_of_array(array, element)
